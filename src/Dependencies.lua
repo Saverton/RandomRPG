b1f3f7cb95dd5b57059cad94edaaa2efc9fd3805
Saverton <scottmeadows04@gmail.com>
2022-08-22 16:08:33 -0400

    Base Code

---
+++ src/Dependencies.lua
@@ -0,0 +1,33 @@
+--
+-- libraries for base Love2D project
+--
+
+Class = require 'lib/class'
+Event = require 'lib/knife.event'
+push = require 'lib/push'
+Timer = require 'lib/knife.timer'
+
+require 'src/constants'
+require 'src/StateMachine'
+require 'src/Util'
+
+require 'src/states/BaseState'
+require 'src/states/StateStack'
+
+gTextures = {
+
+}
+
+gFrames = {
+
+}
+
+gFonts = {
+    ['small'] = love.graphics.newFont('fonts/font.ttf', 8),
+    ['medium'] = love.graphics.newFont('fonts/font.ttf', 16),
+    ['large'] = love.graphics.newFont('fonts/font.ttf', 32)
+}
+
+gSounds = {
+    
+}
---
+++ src/constants.lua
@@ -0,0 +1,10 @@
+--[[
+    Base Constants data file for Love2D projects
+    @author Saverton
+]]
+
+VIRTUAL_WIDTH = 384
+VIRTUAL_HEIGHT = 216
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
