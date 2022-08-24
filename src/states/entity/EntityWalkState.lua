5a037807482c7605dd6b1361dc0b6817ec856ae1
Saverton <scottmeadows04@gmail.com>
2022-08-24 11:16:23 -0400

    Feature collision complete

--- src/states/entity/EntityWalkState.lua
+++ src/states/entity/EntityWalkState.lua
@@ -14,39 +14,44 @@ function EntityWalkState:init(entity)
 end
 
 function EntityWalkState:update(dt)
-    if not(self:checkCollision()) then
-        if self.entity.direction == 'up' then
-            self.entity.y = self.entity.y - (self.entity.speed * dt)
-        elseif self.entity.direction == 'right' then
-            self.entity.x = self.entity.x + (self.entity.speed * dt)
-        elseif self.entity.direction == 'down' then
-            self.entity.y = self.entity.y + (self.entity.speed * dt)
-        elseif self.entity.direction == 'left' then
-            self.entity.x = self.entity.x - (self.entity.speed * dt)
-        end
+    local oldX, oldY = self.entity.x, self.entity.y
+    if self.entity.direction == 'up' then
+        self.entity.y = self.entity.y - (self.entity.speed * dt)
+    elseif self.entity.direction == 'right' then
+        self.entity.x = self.entity.x + (self.entity.speed * dt)
+    elseif self.entity.direction == 'down' then
+        self.entity.y = self.entity.y + (self.entity.speed * dt)
+    elseif self.entity.direction == 'left' then
+        self.entity.x = self.entity.x - (self.entity.speed * dt)
+    end
+
+    -- if the movement causes a collision, move the entity back to its old spot, that way they can move next frame without having to back up.
+    if (self:checkCollision()) then
+        self.entity.x, self.entity.y = oldX, oldY
     end
 
     -- keep entity on map
+    local size = self.entity.level.map.size
     if self.entity.x < 0 then
         self.entity.x = 0
-    elseif self.entity.x + self.entity.width > (self.entity.level.map.size * TILE_SIZE) then
-        self.entity.x = math.floor((self.level.map.size * TILE_SIZE) - self.entity.width)
+    elseif self.entity.x + self.entity.width > (size * TILE_SIZE) then
+        self.entity.x = math.floor((size * TILE_SIZE) - self.entity.width)
     end
 
     if self.entity.y < 0 then
         self.entity.y = 0
-    elseif self.entity.y + self.entity.height > (self.entity.level.map.size * TILE_SIZE) then
-        self.entity.y = math.floor((self.level.map.size * TILE_SIZE) - self.entity.height)
+    elseif self.entity.y + self.entity.height > (size * TILE_SIZE) then
+        self.entity.y = math.floor((size * TILE_SIZE) - self.entity.height)
     end
 end
 
 function EntityWalkState:checkCollision()
     local ent = self.entity
     local tilesToCheck = {}
-    local mapX, mapY, mapXB, mapYB = math.floor(ent.x / TILE_SIZE), math.floor(ent.y / TILE_SIZE), 
-        math.floor((ent.x + ent.width) / TILE_SIZE), math.floor((ent.y + ent.height) / TILE_SIZE)
+    -- add one to each to match feature map indexes
+    local mapX, mapY, mapXB, mapYB = math.floor((ent.x + PLAYER_HITBOX_X_OFFSET) / TILE_SIZE) + 1, math.floor((ent.y + PLAYER_HITBOX_Y_OFFSET) / TILE_SIZE) + 1, 
+        math.floor((ent.x + ent.width + PLAYER_HITBOX_XB_OFFSET) / TILE_SIZE) + 1, math.floor((ent.y + ent.height + PLAYER_HITBOX_YB_OFFSET) / TILE_SIZE) + 1
     local collide = false
-    print(tostring(mapX) .. ', ' .. tostring(mapY) .. ', ' .. tostring(mapXB) .. ', ' .. tostring(mapYB))
 
     if ent.direction == 'up' then
         tilesToCheck = {{mapX, mapY}, {mapXB, mapY} }
@@ -59,11 +64,10 @@ function EntityWalkState:checkCollision()
     end
 
     for i, coord in pairs(tilesToCheck) do
-        print(tostring(coord[1]) .. ', ' .. tostring(coord[2]))
         if coord[1] < 1 or coord[1] > ent.level.map.size or coord[2] < 1 or coord[2] > ent.level.map.size then
             goto continue
         end
-        local feature = ent.level.map.featureMap[coord[1] + 1][coord[2] + 1]
+        local feature = ent.level.map.featureMap[coord[1]][coord[2]]
         if feature ~= nil and feature.isSolid then
             collide = true
             break
