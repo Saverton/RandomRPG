bf7b86ffb8f38f8d36aad4f6423f74e24217bd43
Saverton <scottmeadows04@gmail.com>
2022-09-13 16:01:24 -0400

    yay done all, debug time <:)

--- src/states/game/data/LoadState.lua
+++ src/states/game/data/LoadState.lua
@@ -7,22 +7,22 @@ LoadState = Class{__includes = BaseState}
  
 function LoadState:init(path, loadLevel)
     love.filesystem.setIdentity('random_rpg')
-    self.path = path
-    self.loadLevel = loadLevel or nil
-    self.level = nil
+    self.path = path -- file path to world
+    self.loadLevel = loadLevel or nil -- level to be loaded
+    self.level = nil -- level that is loaded
 end
 
 function LoadState:update(dt)
-    self.level = self:loadWorld()
-
+    self.level = self:loadWorld() -- load the world
     gStateStack:pop()
     gStateStack:push(WorldState({
         level = self.level
-    }))
+    })) -- create the level
     gStateStack:push(DivergePointState({x = self.level.player.x + self.level.player.width / 2 - self.level.camera.x, 
-        y = self.level.player.y + self.level.player.height / 2 - self.level.camera.y}, {0, 0, 0, 1}, 1.5))
+        y = self.level.player.y + self.level.player.height / 2 - self.level.camera.y}, {0, 0, 0, 1}, 1.5)) -- diverge point state to introduce the world
 end
 
+-- render a dark green screen for loading
 function LoadState:render()
     love.graphics.setColor(0, 0, 0, 1)
     love.graphics.rectangle('fill', 0, 0, VIRTUAL_WIDTH, VIRTUAL_HEIGHT)
@@ -30,60 +30,49 @@ end
 
 function LoadState:loadWorld()
     local worldName = string.sub(self.path, 8, string.len(self.path))
-    local map = nil
-    local player = nil
-    local enemySpawner = nil
-    local npcManager = nil
-    local pickupManager = nil
-
-    player = self:loadPlayer()
+    local player = self:loadPlayer()
     if self.loadLevel == nil then
         self.loadLevel = player.currentLevel or 'overworld-1'
     end
-    self.path = self.path .. '/' .. self.loadLevel
+    self.path = self.path .. '/' .. self.loadLevel -- set path to level specific
+    self.levelType = string.sub(self.loadLevel, 0, string.find(self.loadLevel, "-")) -- type of the level being saved
     if not love.filesystem.getInfo(self.path) then
-        return (Level(worldName, self.loadLevel, nil, player))
+        if self.levelType == 'overworld' then
+            return Overworld(worldName, self.loadLevel, {player = player})
+        elseif self.levelType == 'dungeon' then
+            return Dungeon(worldName, self.loadLevel, {player = player})
+        end
     end
-    
-    local playerPos = loadstring(love.filesystem.read(self.path .. '/player_pos.lua'))()
-    player.pos.x = math.floor(playerPos.x / TILE_SIZE)
-    player.pos.y = math.floor(playerPos.y / TILE_SIZE)
-
-    map = self:loadMap()
-    enemySpawner = self:loadEntities()
-    npcManager = self:loadNPCS()
-    pickupManager = self:loadPickups()
-
-    return Level(worldName, self.loadLevel, map, player, enemySpawner, npcManager, pickupManager)
+    local map = self:loadMap()
+    local entityManager = self:loadEntities()
+    local pickupManager = self:loadPickups()
+    if self.levelType == 'overworld' then
+        local playerPosition = loadstring(love.filesystem.read(self.path .. '/player_position.lua'))()
+        player.position.x, player.position.y = math.floor(playerPosition.x / TILE_SIZE), math.floor(playerPosition.y / TILE_SIZE)
+        local npcManager = self:loadNPCS()
+        return Overworld(worldName, self.loadLevel, {map = map, player = player, entityManager = entityManager, 
+            npcManager = npcManager, pickupManager = pickupManager}) -- create an overworld
+    elseif self.levelType == 'dungeon' then
+        return Dungeon(worldName, self.loadLevel, {map = map, player = player, entityManager = entityManager, 
+        pickupManager = pickupManager}) -- create a dungeon
+    end
 end
 
+-- load a map
 function LoadState:loadMap()
     local tileMap = loadstring(love.filesystem.read(self.path .. '/world_tiles.lua'))()
-    local biomeMap = loadstring(love.filesystem.read(self.path .. '/world_biomes.lua'))()
-
     for i, col in ipairs(tileMap) do
         for j, tile in ipairs(col) do
             tileMap[i][j] = Tile(tile)
         end
     end
-
-    for i, col in ipairs(biomeMap) do
-        for j, biome in ipairs(col) do
-            biomeMap[i][j] = Biome(biome)
-        end
-    end
-
-    local size = #tileMap 
+    local width, height = #tileMap, #tileMap[1]
     local featureMap = loadstring(love.filesystem.read(self.path .. '/world_features.lua'))()
     local gatewayMap = loadstring(love.filesystem.read(self.path .. '/world_gateways.lua'))()
-    local spawnerMap = loadstring(love.filesystem.read(self.path .. '/world_spawners.lua'))()
-    local startSpace = loadstring(love.filesystem.read(self.path .. '/start_space.lua'))()
-
-    for col = 1, size, 1 do
-        for row = 1, size, 1 do
+    for col = 1, width, 1 do
+        for row = 1, height, 1 do
             if featureMap[col][row] ~= nil then
-                if FEATURE_DEFS[featureMap[col][row]].spawner then
-                elseif FEATURE_DEFS[featureMap[col][row]].animated then
+                if FEATURE_DEFS[featureMap[col][row]].animated then
                     featureMap[col][row] = AnimatedFeature(featureMap[col][row], Animation(featureMap[col][row], 'main'))
                 else
                     featureMap[col][row] = Feature(featureMap[col][row])
@@ -91,8 +80,19 @@ function LoadState:loadMap()
             end
         end
     end
-
-    return Map(self.loadLevel, size, tileMap, biomeMap, featureMap, gatewayMap, spawnerMap, startSpace)
+    if self.levelType == 'overworld' then
+        local biomeMap = loadstring(love.filesystem.read(self.path .. '/world_biomes.lua'))()
+        for i, col in ipairs(biomeMap) do
+            for j, biome in ipairs(col) do
+                biomeMap[i][j] = Biome(biome)
+            end
+        end
+        return OverworldMap({width = width, height = height}, {biomeMap = biomeMap, tileMap = tileMap, featureMap = featureMap, gatewayMap = gatewayMap})
+    elseif self.levelType == 'dungeon' then
+        local spawnerMap = loadstring(love.filesystem.read(self.path .. '/world_spawners.lua'))()
+        local start = loadstring(love.filesystem.read(self.path .. '/start.lua'))()
+        return DungeonMap({width = width, height = height}, {tileMap = tileMap, featureMap = featureMap, gatewayMap = gatewayMap, start = start, spawnerMap = spawnerMap})
+    end
 end
 
 function LoadState:loadPlayer()
