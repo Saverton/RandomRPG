437e18708d427b99c8370a93f460ecf02f6635de
Saverton <scottmeadows04@gmail.com>
2022-09-22 19:30:12 -0400

    made mountain chests rarer

--- src/level/map/biomes/biome_defs.lua
+++ src/level/map/biomes/biome_defs.lua
@@ -51,11 +51,11 @@ BIOME_DEFS = {
         features = {
             {
                 name = 'rock', 
-                chance = 0.995
+                chance = 0.999
             },
             {
                 name = 'chest',
-                chance = 0.005
+                chance = 0.001
             }
         },
         featProc = 0.5
